6c96a9a12f52e1b56009fbde76f95e45447a2551
Christian Francia <CTFrancia@gmail.com>
2019-01-15 19:59:31 +0100

    sort tournament list by date

--- barna-ajedrez/src/components/TournamentComponents/tournamentList.js
+++ barna-ajedrez/src/components/TournamentComponents/tournamentList.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 import TournamentItem from './tournamentitem';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import moment from 'moment';
+// moment.format();
 
 
 class TournamentList extends Component {
@@ -18,6 +20,23 @@ class TournamentList extends Component {
     if (tournament === undefined) {
       return <div> <LinearProgress /> </div>;
     } else {
+      tournament.data.sort((a,b) => {
+        let arr1 = a.startDate.split('-').slice(0, 10);
+        let arr2 = b.startDate.split('-').slice(0, 10);
+        let aDD = arr1[0];
+        let aMM = arr1[1];
+        let aYYYY = arr1[2];
+        let bDD = arr2[0];
+        let bMM = arr2[1];
+        let bYYYY = arr2[2];
+        console.log(arr1, arr2,'ARRAYS');
+        return new Date(aYYYY, aMM, aDD) - new Date(bYYYY, bMM, bDD);
+      })
+      .filter(el => {
+        const today = moment()._d;
+        console.log('FILTER ELEMET',el, 'DATE', today );
+
+      });
       return (
         <div id="ti-container">
           {tournament.data.map(tournamentEl => (
